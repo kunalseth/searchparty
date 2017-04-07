53603249afcb16862983db2122ea55a4d6ecfaeb
aparnard <aparna.rd09@gmail.com>
2017-04-07 01:16:31 -0700

    Adding file prelim.py which extracts necessary data from the excel input file and outputs to data.csv Data looks very clean

---
+++ prelim.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+xl = pd.ExcelFile("data.xlsx")
+df = xl.parse("Data")
+
+
+df.reset_index(inplace=True) 	# to remove multi level index
+new_header = df_copy.iloc[6]	# rows 0-5 are not needed. header present in row 6
+df=df.rename(columns = new_header) # rename columns in data frame
+df = df[7:]  # remove unwanted rows
+
+df.to_csv("data.csv")
