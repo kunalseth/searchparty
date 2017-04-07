178f5dcda9ee235b7852fc939dcf9b79bf97affe
aparnard <aparna.rd09@gmail.com>
2017-04-07 01:34:26 -0700

    Fixing error

--- prelim.py
+++ prelim.py
@@ -5,7 +5,7 @@ df = xl.parse("Data")
 
 
 df.reset_index(inplace=True) 	# to remove multi level index
-new_header = df_copy.iloc[6]	# rows 0-5 are not needed. header present in row 6
+new_header = df.iloc[6]	# rows 0-5 are not needed. header present in row 6
 df=df.rename(columns = new_header) # rename columns in data frame
 df = df[7:]  # remove unwanted rows
 
